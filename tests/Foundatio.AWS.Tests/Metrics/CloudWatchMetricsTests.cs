e306ded06d03e201c19f4a1a9d777de88ea21316
Eric J. Smith <eric@codesmithtools.com>
2020-04-24 21:42:40 -0500

    Fix build warning

--- tests/Foundatio.AWS.Tests/Metrics/CloudWatchMetricsTests.cs
+++ tests/Foundatio.AWS.Tests/Metrics/CloudWatchMetricsTests.cs
@@ -15,15 +15,18 @@ namespace Foundatio.AWS.Tests.Metrics {
         }
 
         public override IMetricsClient GetMetricsClient(bool buffered = false) {
+            // Don't run this as part of the tests because it doesn't work reliably since CloudWatch can take a long time for the stats to show up.	
+            // Also, you can't delete metrics so we have to use random ids and it creates a bunch of junk data.	
             return null;
             
-            // ReSharper disable once HeuristicUnreachableCode
+#pragma warning disable CS0162 // Unreachable code detected
             string id = Guid.NewGuid().ToString("N").Substring(0, 10);
             return new CloudWatchMetricsClient(
                 o => o.ConnectionString($"serviceurl=http://localhost:4566;Test Id={id}")
                     .Prefix("foundatio/tests/metrics")
                     .Buffered(buffered)
                     .LoggerFactory(Log));
+#pragma warning restore CS0162 // Unreachable code detected
         }
 
         [Fact]
